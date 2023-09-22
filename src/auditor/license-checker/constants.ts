c393aab8fdf7b0063a424cc33ccb2a7805e5e09c
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-22 17:26:51 -0500

    Overcome a module mess with jest config.

--- src/auditor/license-checker/constants.ts
+++ src/auditor/license-checker/constants.ts
@@ -3,8 +3,7 @@ import { readFileSync } from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const templates: Record<string, string> = {
   [readFileSync(`${__dirname}/templates/BSD-2-Clause.txt`).toString()]:
