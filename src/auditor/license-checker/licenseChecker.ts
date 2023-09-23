063f6dacd083bcc28406308d487dd48ca53a313c
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-23 14:34:07 -0500

    Expand unit tests and make duplicate removal compliant with all edge cases.

--- src/auditor/license-checker/licenseChecker.ts
+++ src/auditor/license-checker/licenseChecker.ts
@@ -161,12 +161,47 @@ export const removeDuplicates = (licenseData: License[]): License[] => {
   const result: License[] = [];
   licenseData.forEach((license) => {
     if (
-      !result.some(
-        (l) =>
+      !result.some((l) => {
+        let licensesMatch = false;
+
+        if (Array.isArray(l.licenses) && Array.isArray(license.licenses)) {
+          // if both licenses are arrays, check if all licenses in l.licenses
+          // are included in license.licenses
+          licensesMatch = l.licenses.every((x) => license.licenses.includes(x));
+        } else if (
+          !Array.isArray(l.licenses) &&
+          !Array.isArray(license.licenses)
+        ) {
+          // if both licenses are not arrays, check if they are equal
+          licensesMatch = l.licenses === license.licenses;
+        } else {
+          // if one of the licenses is an array and the other is not
+          if (
+            Array.isArray(l.licenses) &&
+            l.licenses.length == 1 &&
+            !Array.isArray(license.licenses)
+          ) {
+            // if l.licenses is an array with one element, check if license.licenses
+            // is included in l.licenses
+            licensesMatch = l.licenses.includes(license.licenses);
+          } else if (
+            !Array.isArray(l.licenses) &&
+            Array.isArray(license.licenses) &&
+            license.licenses.length == 1
+          ) {
+            // if license.licenses is an array with one element, check if l.licenses
+            // is included in license.licenses
+            licensesMatch = license.licenses.includes(l.licenses);
+          }
+        }
+
+        // check if name, version and licenses match
+        return (
           l.name === license.name &&
           l.version === license.version &&
-          l.licenses === license.licenses
-      )
+          licensesMatch
+        );
+      })
     ) {
       result.push(license);
     }
--- tests/auditor/license-checker/licenseChecker.test.ts
+++ tests/auditor/license-checker/licenseChecker.test.ts
@@ -4,7 +4,8 @@ import { License } from "../../../src/models/license";
 // removeDuplicates should remove duplicate licenses based on common name,
 // version and licenses array
 describe("removeDuplicates", () => {
-  it("should remove duplicate licenses based on common name, version and licenses array", () => {
+  // test based on licenses property when used a string
+  it("should remove duplicate licenses based on common name, version and licenses property when licenses properties are both strings", () => {
     const licenseData: License[] = [
       {
         name: "test",
@@ -33,5 +34,133 @@ describe("removeDuplicates", () => {
     const result = removeDuplicates(licenseData);
 
     expect(result.length).toBe(1);
+  });
+
+  // test based on licenses property when used an array
+  it("should remove duplicate licenses based on common name, version and licenses array", () => {
+    const licenseData: License[] = [
+      {
+        name: "test",
+        version: "1.0.0",
+        licenses: ["MIT"],
+        path: "test",
+        licensePath: "test",
+        repository: "test",
+        publisher: "test",
+        email: "test",
+        rootProjectName: "test",
+      },
+      {
+        name: "test",
+        version: "1.0.0",
+        licenses: ["MIT"],
+        path: "test-2",
+        licensePath: "test-2",
+        repository: "test-2",
+        publisher: "test-2",
+        email: "test-2",
+        rootProjectName: "test",
+      },
+    ];
+
+    const result = removeDuplicates(licenseData);
+
+    expect(result.length).toBe(1);
+  });
+
+  // test based on licenses property when used an array with different values
+  it("should remove duplicate licenses based on common name, version and licenses arrays have different values", () => {
+    const licenseData: License[] = [
+      {
+        name: "test",
+        version: "1.0.0",
+        licenses: ["MIT", "Apache-2.0"],
+        path: "test",
+        licensePath: "test",
+        repository: "test",
+        publisher: "test",
+        email: "test",
+        rootProjectName: "test",
+      },
+      {
+        name: "test",
+        version: "1.0.0",
+        licenses: ["MIT"],
+        path: "test-2",
+        licensePath: "test-2",
+        repository: "test-2",
+        publisher: "test-2",
+        email: "test-2",
+        rootProjectName: "test",
+      },
+    ];
+
+    const result = removeDuplicates(licenseData);
+
+    expect(result.length).toBe(2);
+  });
+
+  // test based on licenses property when some objects have a string and others an array
+  it("should remove duplicate licenses based on common name, version and licenses property when some are strings and others are arrays", () => {
+    const licenseData: License[] = [
+      {
+        name: "test",
+        version: "1.0.0",
+        licenses: "MIT",
+        path: "test",
+        licensePath: "test",
+        repository: "test",
+        publisher: "test",
+        email: "test",
+        rootProjectName: "test",
+      },
+      {
+        name: "test",
+        version: "1.0.0",
+        licenses: ["MIT"],
+        path: "test-2",
+        licensePath: "test-2",
+        repository: "test-2",
+        publisher: "test-2",
+        email: "test-2",
+        rootProjectName: "test",
+      },
+    ];
+
+    const result = removeDuplicates(licenseData);
+
+    expect(result.length).toBe(1);
+  });
+
+  // test based on licenses property when some objects have a string and others an array with different values
+  it("should remove duplicate licenses based on common name, version and licenses property when some are arrays and others are strings", () => {
+    const licenseData: License[] = [
+      {
+        name: "test",
+        version: "1.0.0",
+        licenses: "MIT",
+        path: "test",
+        licensePath: "test",
+        repository: "test",
+        publisher: "test",
+        email: "test",
+        rootProjectName: "test",
+      },
+      {
+        name: "test",
+        version: "1.0.0",
+        licenses: ["MIT", "Apache-2.0"],
+        path: "test-2",
+        licensePath: "test-2",
+        repository: "test-2",
+        publisher: "test-2",
+        email: "test-2",
+        rootProjectName: "test",
+      },
+    ];
+
+    const result = removeDuplicates(licenseData);
+
+    expect(result.length).toBe(2);
   });
 });
