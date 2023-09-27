8ec1b754a90a0a02ecac370b61a23f0b97e7dfde
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-27 17:26:30 -0500

    Update for tests.

--- src/util/root-project.ts
+++ src/util/root-project.ts
@@ -13,9 +13,15 @@ export function getRootProjectName(pathToProject: string) {
 
 // return the version string from this module's package.json
 export function getCurrentVersionString() {
-  const packageJson = JSON.parse(
-    readFileSync(`${__dirname}/../../../package.json`).toString()
-  );
-  const version = packageJson?.version ?? "UNKNOWN";
-  return version;
+  try {
+    const packageJson = JSON.parse(
+      readFileSync(`${__dirname}/../../../package.json`).toString()
+    );
+    const version = packageJson?.version ?? "UNKNOWN";
+    return version;
+  } catch (e) {
+    // this error happens when running the tests
+    // which have a different path relationship to the package.json file
+    return "UNKNOWN";
+  }
 }
