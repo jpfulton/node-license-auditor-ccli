8ff72ba70ec08049e0e58f70e024e7c395f50028
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-28 17:01:42 -0500

    Update configuration module to fetch remote config files.

--- src/models/configuration.ts
+++ src/models/configuration.ts
@@ -1,5 +1,11 @@
+// configuration source type is either "file" or "default"
+// "file" means the configuration was loaded from a file
+// "default" means the configuration was loaded from the default configuration
+declare type ConfigurationSourceType = "remote" | "file" | "default";
+
 export interface Configuration {
   whiteList: string[];
   blackList: string[];
-  configurationSource?: "default" | "file";
+  configurationSource: ConfigurationSourceType;
+  configurationFileName?: string;
 }
--- src/util/configuration.ts
+++ src/util/configuration.ts
@@ -1,4 +1,5 @@
 import { readFile } from "fs/promises";
+import fetch from "node-fetch";
 import blacklist from "../default-configuration/blacklist.js";
 import whitelist from "../default-configuration/whitelist.js";
 import { Configuration } from "../models/configuration.js";
@@ -9,11 +10,12 @@ const defaultConfiguration: Configuration = {
   whiteList: whitelist,
   blackList: blacklist,
   configurationSource: "default",
+  configurationFileName: undefined,
 };
 
 // return a configuration object from a file
 // if the file does not exist, return the default configuration
-export default async function getConfiguration(
+export async function getConfiguration(
   configFileName: string = DEFAULT_CONFIG_FILE_NAME
 ): Promise<Configuration> {
   try {
@@ -24,9 +26,34 @@ export default async function getConfiguration(
       await readFile(`${process.cwd()}/${configFileName}`, "utf8")
     ) as Configuration;
     configuration.configurationSource = "file";
+    configuration.configurationFileName = configFileName;
 
     return configuration;
   } catch (e) {
     return defaultConfiguration;
   }
+}
+
+// return a configuration object from a remote file based on a URL over HTTP or HTTPS
+// if the file does not exist, throw an error
+export async function getConfigurationFromUrl(
+  url: string
+): Promise<Configuration> {
+  // read the configuration JSON file into a Configuration object
+  // pull the file from the URL provided by the url parameter
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(
+      `Unable to load configuration from URL: ${url} Status: ${response.status} was returned.`
+    );
+  }
+
+  const body = await response.text();
+
+  const configuration = JSON.parse(body) as Configuration;
+  configuration.configurationSource = "remote";
+  configuration.configurationFileName = url;
+
+  return configuration;
 }
